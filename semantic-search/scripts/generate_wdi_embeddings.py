096aad897dbcf0b30b254f9fa215c91765031c93
Aivin V. Solatorio <avsolatorio@gmail.com>
2024-04-24 18:58:14 -0400

    Update embedding generator
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- semantic-search/scripts/generate_wdi_embeddings.py
+++ semantic-search/scripts/generate_wdi_embeddings.py
@@ -1,21 +1,33 @@
+import os
+import pandas as pd
+import fire
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer('avsolatorio/GIST-all-MiniLM-L6-v2')
 
-# Load the WDI dataset
-import pandas as pd
+def main(model_name: str = "avsolatorio/GIST-all-MiniLM-L6-v2"):
+    _, base_model = model_name.split("/")
+    data_dir = os.path.join(os.path.dirname(__file__), '../data')
 
-wdi = pd.read_excel('semantic-search/data/WDIEXCEL.xlsx', sheet_name='Series')
+    model = SentenceTransformer(model_name)
 
-wdi["definition"] = wdi["Long definition"]
-wdi.loc[wdi["definition"].isnull(), "definition"] = wdi.loc[wdi["definition"].isnull(), "Short definition"]
+    # Load the WDI dataset
+    wdi = pd.read_excel(os.path.join(data_dir, "WDIEXCEL.xlsx"), sheet_name='Series')
 
-# Generate embeddings
-text_input = wdi['Indicator Name'] + '\n\n' + wdi['definition']
+    # Generate input text
+    wdi["definition"] = wdi["Long definition"]
+    wdi.loc[wdi["definition"].isnull(), "definition"] = wdi.loc[wdi["definition"].isnull(), "Short definition"]
 
-wdi['embedding'] = model.encode(text_input).tolist()
+    text_input = wdi['Indicator Name'] + '\n\n' + wdi['definition']
 
-# Save the embeddings
-wdi.rename(columns={"Series Code": "code", "Indicator Name": "name"}, inplace=True)
+    # Generate embeddings
+    wdi['embedding'] = model.encode(text_input, show_progress_bar=True).tolist()
 
-wdi[["code", "name", "embedding"]].to_json("semantic-search/data/wdi_embeddings.json", orient="records")
+    # Process and save the embeddings
+    wdi.rename(columns={"Series Code": "code", "Indicator Name": "name"}, inplace=True)
+
+    wdi[["code", "name", "embedding"]].to_json(os.path.join(data_dir, f"{base_model}__wdi_embeddings.json"), orient="records")
+
+
+if __name__ == "__main__":
+    # poetry run python semantic-search/scripts/generate_wdi_embeddings.py --model_name="avsolatorio/GIST-all-MiniLM-L6-v2"
+    fire.Fire(main)
