028016a632e8767ae4333baba34bdd1c38d81891
Aivin V. Solatorio <avsolatorio@gmail.com>
2024-04-26 23:10:42 -0400

    Use parcel search.html to build
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- semantic-search/js/search/search.js
+++ semantic-search/js/search/search.js
@@ -1,4 +1,10 @@
 import bm25 from 'wink-bm25-text-search';
 import similarity from 'compute-cosine-similarity';
 
+console.log("Hello similarity...")
+console.log(similarity)
+
+console.log("Hello bm25...")
+console.log(bm25)
+
 export { bm25, similarity }
