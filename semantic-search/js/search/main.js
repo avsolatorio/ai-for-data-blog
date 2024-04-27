a97eda897c822e5c7f21dba0de425e15db1c6f1c
Aivin V. Solatorio <avsolatorio@gmail.com>
2024-04-27 01:37:24 -0400

    Update search with lexical model package
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- semantic-search/js/search/main.js
+++
@@ -1,4 +0,0 @@
-var bm25 = require('wink-bm25-text-search');
-var similarity = require('compute-cosine-similarity');
-
-module.exports = { bm25, similarity }
--- semantic-search/js/search/search.js
+++ semantic-search/js/search/search.js
@@ -1,10 +1,26 @@
 import bm25 from 'wink-bm25-text-search';
 import similarity from 'compute-cosine-similarity';
+import winkNLP from 'wink-nlp';
+import winkModel from 'wink-eng-lite-web-model';
 
-console.log("Hello similarity...")
-console.log(similarity)
+// Test similarity
+var cosSim = similarity([1, 2], [2, 3]);
+console.log(cosSim);
 
-console.log("Hello bm25...")
-console.log(bm25)
+const nlp = winkNLP(winkModel);
+const its = nlp.its;
 
-export { bm25, similarity }
+const prepTask = function (text) {
+    const tokens = [];
+    nlp.readDoc(text)
+        .tokens()
+        // Use only words ignoring punctuations etc and from them remove stop words
+        .filter((t) => (t.out(its.type) === 'word' && !t.out(its.stopWordFlag)))
+        // Handle negation and extract stem of the word
+        .each((t) => tokens.push((t.out(its.negationFlag)) ? '!' + t.out(its.stem) : t.out(its.stem)));
+
+    return tokens;
+};
+
+// Expose packages
+export { similarity, bm25, winkNLP, winkModel, prepTask, nlp }
